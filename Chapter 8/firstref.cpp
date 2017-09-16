6a39be473f9d8a99e874f829b4de63b1b98c136e
maxiaoan <maxiaoansuccess@qq.com>
2017-09-16 17:45:51 +0800

    update

--- Chapter 8/firstref.cpp
+++ Chapter 8/firstref.cpp
@@ -4,8 +4,10 @@ int main()
 {
     using namespace std;
     int rats = 101;
+    //类型标识符，指的是指向int的引用。
+    
     int & rodents = rats;   // rodents is a reference
-
+    cout << "rats address = " << &rats<<endl ;
     cout << "rats = " << rats;
     cout << ", rodents = " << rodents << endl;
     rodents++;
@@ -14,6 +16,7 @@ int main()
 
 // some implementations require type casting the following
 // addresses to type unsigned
+    //rats和rodents的地址是一样的
     cout << "rats address = " << &rats;
     cout << ", rodents address = " << &rodents << endl;
     // cin.get();
